5c490ca0b733a8bc41a1d9c87152e75c3bdc0371
NURRABBY SHUVO <106588083+0xnurrabby@users.noreply.github.com>
2025-12-21 23:57:24 +0600

    Update main.js

--- src/main.js
+++ src/main.js
@@ -15,7 +15,7 @@ import { Attribution } from "https://esm.sh/ox/erc8021";
 // =====================================================
 // HARD INPUTS
 // =====================================================
-const TOP_TITLE = "For .... clcik ☰";
+const TOP_TITLE = "👈👈🏿👈🏻clcik here for ...";
 const HUD_TITLE = "Live Statistics";
 const HOME_URL = "https://web3-runner.vercel.app/";
 
